b53a13db6a3f197dc8eb2a35f62bb5321b22f758
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-09 19:08:50 +0100

    Basic security implemented with in-memory defined users.

---
+++ src/main/java/dev/ned/springsecurity/demo/config/security/SpringSecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package dev.ned.springsecurity.demo.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
